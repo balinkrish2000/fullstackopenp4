1cb1a14d6b1cf3a9beb91ca9703ddddf9cd6644b
balinkrish2000 <51079927+balinkrish2000@users.noreply.github.com>
2023-08-28 07:16:02 +0000

    4.1 Complete

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://fullstack:fullstackopen@fullstackopen.skrakrw.mongodb.net/blogList?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+blogSchema.set('toJSON', {
+    transform: (doument, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString(),
+        delete returnedObject._id,
+        delete returnedObject.__v
+
+    }
+})
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
