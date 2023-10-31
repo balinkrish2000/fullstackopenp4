01a67b78a9b94430a211ffa205769e707d3e06ec
balinkrish2000 <51079927+balinkrish2000@users.noreply.github.com>
2023-10-31 10:52:11 +0000

    4.13 Complete

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -13,75 +13,91 @@ beforeEach(async () => {
         .map(blog => new Blog(blog))
     const PromiseArray = blogObjects.map(blog => blog.save())
     await Promise.all(PromiseArray)
-}
-
-)
-
-test('blogs are returned as JSON', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
 })
 
-test('verifying unique property of blog', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
-},100000)
-
-test('a valid blog can be added', async () => {
-    const newBlog =  {
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDB()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-    
-    const contents = blogsAtEnd.map(({id, ...blog}) => blog) 
-    expect(contents).toContainEqual(newBlog)
-},100000)
-
-test('defaulting likes property value', async () => {
-    const newBlog =  {
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    
-    const blogsAtEnd = await helper.blogsInDB()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-    
-    const updatedBlog = blogsAtEnd.filter(({id, ...blog}) => {
-        if (blog.title == "Canonical string reduction") return blog
+describe('reading from blog list', () => {
+    test('blogs are returned as JSON', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
     })
-    expect(updatedBlog[0].likes).toBe(0)
+    
+    test('verifying unique property of blog', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    },100000)    
 })
 
-test("reject blog list with title or url missing", async () => {
-    const newBlog =  {
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
-    }
+describe('adding to blog List', () => {
+    test('a valid blog can be added', async () => {
+        const newBlog =  {
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5
+        }
     
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+    
+        const blogsAtEnd = await helper.blogsInDB()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        
+        const contents = blogsAtEnd.map(({id, ...blog}) => blog) 
+        expect(contents).toContainEqual(newBlog)
+    },100000)
+    
+    test('defaulting likes property value', async () => {
+        const newBlog =  {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
+        }
+    
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        const blogsAtEnd = await helper.blogsInDB()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        
+        const updatedBlog = blogsAtEnd.filter(({id, ...blog}) => {
+            if (blog.title == "Canonical string reduction") return blog
+        })
+        expect(updatedBlog[0].likes).toBe(0)
+    })
+    
+    test("reject blog list with title or url missing", async () => {
+        const newBlog =  {
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
+        }
+        
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
+})
+
+describe('deleting from blog list', () => {
+    test('delete a blog that exists in the list', async () => {
+        const blogsAtStart = await helper.blogsInDB()
+        const blogToDelete = blogsAtStart[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+        
+        const blogsAtEnd = await helper.blogsInDB()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+    })
 })
 
 afterAll(async () => {
