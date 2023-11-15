ee10378ecd9ac8ecbd313cf6c6ac3b22bf39fc24
balinkrish2000 <51079927+balinkrish2000@users.noreply.github.com>
2023-11-15 06:23:39 +0000

    4.15 Complete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 
@@ -16,7 +17,10 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
