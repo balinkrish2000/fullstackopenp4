bdde870f229a347d5edad0944810438abe125fca
balinkrish2000 <51079927+balinkrish2000@users.noreply.github.com>
2023-11-15 10:24:56 +0000

    4.17 Complete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,23 @@
 const blogsRouter = require('express').Router()
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
   })
   
   blogsRouter.post('/', async (request, response, next) => {
-    const blog = new Blog(request.body)
+    const blog = request.body
+
+    userList = await User.find({})
+    blog.user = userList[0]._id
     
-    const savedBlog = await blog.save()
+    const newBlog = new Blog(blog)
+
+    const savedBlog = await newBlog.save()
+    userList[0].blogs = userList[0].blogs.concat(savedBlog._id)
+    await userList[0].save()
     response.status(201).json(savedBlog)
   })
 
